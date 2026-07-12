740cb7115718597dcb2f14c69f9fa20da041dab9
gagantirumareddy <gagantirumareddy@gmail.com>
2026-07-12 17:30:12 +0530

    debug and fixed the logo issue

--- CampusHire/frontend/src/components/layout/Header.jsx
+++ CampusHire/frontend/src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
   const location = useLocation();
@@ -32,7 +33,7 @@ const Header = () => {
           </button>
           <Link to="/" className="flex items-center gap-2">
     <img
-        src="/logo.png"
+        src={logo}
         alt="CampusHire Logo"
         className="w-10 h-10"
     />
